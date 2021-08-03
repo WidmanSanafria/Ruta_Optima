489b09f81b5a059e49708446847f21dae4b596a1
Widman Sanafria <si.widmangsm22@uniandes.edu.ec>
2021-08-02 21:42:02 -0500

    primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var google;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+
+  map: any;
+  directionsService = new google.maps.DirectionsService();
+  directionsDisplay = new google.maps.DirectionsRenderer();
+
 
   constructor() {}
+  ngOnInit() {
+    this.loadMap();
+  }
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map');
+    // create LatLng object
+    const myLatLng = {lat: 0.3565299362292779, lng: -78.11755744710119};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
 
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+      const marker = {
+        position:{
+          lat: 0.3565299362292779,
+          lng: -78.11755744710119
+        },
+        title: 'Punto uno'
+      };
+      this.addMarker(marker);
+  });
+  }
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
 }
